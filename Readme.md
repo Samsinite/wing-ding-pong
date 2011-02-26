88470ba2e707e7086d937ea5103c6fa86713428e
Sam Clopton <samsinite@gmail.com>
2011-02-25 17:08:59 -0800

    First Commit

---
+++ Readme.md
@@ -0,0 +1,6 @@
+{wing-ding-pong}
+================
+
+### DESCRIPTION
+
+A pong game for XNA that is part of our WSU Cpts 323 Project.
---
+++ wing-ding-pong/wing-ding-pong/Properties/AssemblyInfo.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("wing-ding-pong")]
+[assembly: AssemblyProduct("wing-ding-pong")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCompany("None")]
+[assembly: AssemblyCopyright("Copyright © None 2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type. Only Windows
+// assemblies support COM.
+[assembly: ComVisible(false)]
+
+// On Windows, the following GUID is for the ID of the typelib if this
+// project is exposed to COM. On other platforms, it unique identifies the
+// title storage container when deploying this assembly to the device.
+[assembly: Guid("dd212b31-8938-45a5-bfdc-93c46e3aa387")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
